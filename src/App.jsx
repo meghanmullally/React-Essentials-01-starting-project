4e68973b63a619e9c8c132f5a746824bb967838d
Meghan Mullally <ms.mullally27@gmail.com>
2023-10-25 17:11:14 -0700

    make separate component files and created a components folder to store them.

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,6 @@
-import reactImg from './assets/react-core-concepts.png';
-import componentsImg from './assets/components.png';
 import { CORE_CONCEPTS } from './data.js';
-
-
-const reactDescriptions = ['Fundamental', 'Crucial', 'Core'];
-
-function genRandomInt(max) {
-  return Math.floor(Math.random() * (max + 1));
-}
-
-function Header() {
-  const description = reactDescriptions[genRandomInt(2)]
-
-  return (
-    <header>
-      <img src={reactImg} alt="Stylized atom" />
-      <h1>React Essentials</h1>
-      <p>
-        {description} React concepts you will need for almost any app you are going to build!
-      </p>
-    </header>
-  );
-}
-
-function CoreConcept(props) {
-  return (
-    <li>
-      <img src={props.image} alt={props.title} />
-      <h3>{props.title}</h3>
-      <p>{props.description}</p>
-    </li>
-  );
-
-}
+import Header from './components/Header';
+import CoreConcept from './components/CoreConcept';
 
 function App() {
   return (
@@ -47,21 +15,12 @@ function App() {
               description={CORE_CONCEPTS[0].description}
               image={CORE_CONCEPTS[0].image}
             />
-            <CoreConcept
-              title={CORE_CONCEPTS[1].title}
-              description={CORE_CONCEPTS[1].description}
-              image={CORE_CONCEPTS[1].image}
-            />
-            <CoreConcept
-              title={CORE_CONCEPTS[2].title}
-              description={CORE_CONCEPTS[2].description}
-            image={CORE_CONCEPTS[2].image}
-            />
-            <CoreConcept
-              title={CORE_CONCEPTS[3].title}
-              description={CORE_CONCEPTS[3].description}
-              image={CORE_CONCEPTS[3].image}
-            />
+
+          {/* alternative to the above code */}
+           <CoreConcept {...CORE_CONCEPTS[1]} />
+           <CoreConcept {...CORE_CONCEPTS[2]} />
+           <CoreConcept {...CORE_CONCEPTS[3]} />
+          
           </ul>
         </section>
         <h2>Time to get started!</h2>
---
+++ src/components/CoreConcept.jsx
@@ -0,0 +1,23 @@
+// function CoreConcept(props) {
+//   return (
+//     <li>
+//       <img src={props.image} alt={props.title} />
+//       <h3>{props.title}</h3>
+//       <p>{props.description}</p>
+//     </li>
+//   );
+
+// }
+
+// another alternative by destructoring, using less code..removing "props" 
+// - note this one is better to use since you'll automatically assume its coming from props anyway
+export default function CoreConcept({image, title, description}) {
+    return (
+      <li>
+        <img src={image} alt={title} />
+        <h3>{title}</h3>
+        <p>{description}</p>
+      </li>
+    );
+  
+  }
