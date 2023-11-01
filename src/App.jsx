f251a816127796ad21b40f9d4b9a0683c62bf4ba
Meghan Mullally <ms.mullally27@gmail.com>
2023-11-01 14:40:47 -0700

    map the data instead to be more dynamic

--- src/App.jsx
+++ src/App.jsx
@@ -40,19 +40,26 @@ function App() {
       <main>
         <section id="core-concepts">
           <h2>Core concepts</h2>
+          {/* map throught data to dynamically generate the list*/}
           <ul>
+          {CORE_CONCEPTS.map((conceptItem) =>
+          // key prop to tell the items a part
+            (<CoreConcept key={conceptItem.title} {...conceptItem} />
+          ))}
+          </ul>
+          {/* <ul>
             <CoreConcept
               title={CORE_CONCEPTS[0].title}
               description={CORE_CONCEPTS[0].description}
               image={CORE_CONCEPTS[0].image}
-            />
+            /> */}
 
-            {/* alternative to the above code */}
-            <CoreConcept {...CORE_CONCEPTS[1]} />
+          {/* alternative to the above code */}
+          {/* <CoreConcept {...CORE_CONCEPTS[1]} />
             <CoreConcept {...CORE_CONCEPTS[2]} />
             <CoreConcept {...CORE_CONCEPTS[3]} />
 
-          </ul>
+          </ul> */}
         </section>
         <section id="examples">
           <h2>Examples</h2>
