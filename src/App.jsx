8bd76a492ee2f97a274517e1b4c2e9437f45ec32
Meghan Mullally <ms.mullally27@gmail.com>
2023-11-01 14:07:17 -0700

    updated data.js file from course, buttons show data onclick

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,21 @@
-import { CORE_CONCEPTS } from './data.js';
+import { useState } from 'react';
+
+import { CORE_CONCEPTS, EXAMPLES } from './data.js';
 import Header from './components/Header';
 import CoreConcept from './components/CoreConcept';
 import TabButton from './components/TabButton.jsx';
 
 function App() {
 
-  function handleSelect() {
-    console.log('Hello World! - selected');
+  const [selectedTopic, setSelectedTopic] = useState('components');
+
+  function handleSelect(selectedButton) {
+    // selectedButton => 'components', 'jsx', 'props, 'state'
+    setSelectedTopic(selectedButton);
+    console.log(setSelectedTopic);
   }
 
+  console.log('APP COMP RENDERING')
 
   return (
     <div>
@@ -33,12 +40,20 @@ function App() {
         <section id="examples">
           <h2>Examples</h2>
           <menu>
-            <TabButton onSelect={handleSelect}>Components</TabButton>
-            <TabButton onSelect={handleSelect}>JSX</TabButton>
-            <TabButton onSelect={handleSelect}>Props</TabButton>
-            <TabButton onSelect={handleSelect}>State</TabButton>
+            <TabButton onSelect={() => handleSelect('components')}>Components</TabButton>
+            <TabButton onSelect={() => handleSelect('jsx')}>JSX</TabButton>
+            <TabButton onSelect={() => handleSelect('props')}>Props</TabButton>
+            <TabButton onSelect={() => handleSelect('state')}>State</TabButton>
           </menu>
-
+          <div id="tab-content">
+            <h3>{EXAMPLES[selectedTopic].title}</h3>
+            <p>{EXAMPLES[selectedTopic].description}</p>
+            <pre>
+              <code>
+              {EXAMPLES[selectedTopic].code}
+              </code>
+            </pre>
+          </div>
         </section>
       </main>
     </div>
